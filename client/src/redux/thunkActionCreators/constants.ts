10ed6bd9dc10d0f6585028acd5b12a10e78ea57b
Carlos Josiel Hernandez Sanchez <e-cjhernandezs@neoris.com>
2020-01-07 22:23:08 -0600

    change APIURL

--- client/src/redux/thunkActionCreators/constants.ts
+++ client/src/redux/thunkActionCreators/constants.ts
@@ -1,3 +1,3 @@
 //export const SERVER = process.env.PORT?'https://wizcoach.herokuapp.com':'http://localhost:3001';
-export const SERVERAPI = 'https://wizcoach.herokuapp.com';
+export const SERVERAPI = 'https://apiwiz.herokuapp.com/';
 export const LOCALSERVER = 'http://localhost:3001';
